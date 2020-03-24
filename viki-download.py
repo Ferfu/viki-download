0d4735cf8281f03582d5851d924031b2049952e9
George Tian <georgetian3@gmail.com>
2020-03-24 16:11:47 +1000

    Finished viki-download.py

--- viki-download.py
+++ viki-download.py
@@ -1,1 +1,38 @@
-import selenium
+import youtube_dl
+
+while True:
+    ep1 = input('Copy + paste the URL of the FIRST episode of the series:\n')
+    try:
+        epUrl = ep1[ep1.index('videos/') + 7:]
+        break
+    except:
+        print('Not a valid URL. The URL should be in the following format:\nhttps://www.viki.com/videos/XXXXXXXv-name-of-drama-episode-XX')
+
+epTag = int(epUrl[:epUrl.index('v-')])
+epName = epUrl[epUrl.index('v-') + 2 : epUrl.index('episode-') - 1]
+
+epList = []
+while True:
+    epNumber = input("List the episode(s) you wish to download in one of the following formats:\n1. Separated by commas, e.g. 1,2,3\n2. Give a range of episodes separated by a dash, e.g 1-3\n")
+    try:
+        if '-' in epNumber:
+            start = int(epNumber[:epNumber.index('-')])
+            end = int(epNumber[epNumber.index('-') + 1:])
+            for i in range(start, end + 1):
+                epList.append('https://www.viki.com/videos/' + str(epTag - 1 + i) + 'v-' + epName + '-episode-' + str(i))
+        elif ',' in epNumber:
+            epNumber = list(epNumber)
+            epNumberList = []
+            for x in epNumber:
+                if x != ' ' and x != ',':
+                    epNumberList.append(int(x))
+            for x in epNumberList:
+                epList.append('https://www.viki.com/videos/' + str(epTag - 1 + x) + 'v-' + epName + '-episode-' + str(x))
+        else:
+            epList.append('https://www.viki.com/videos/' + str(epTag - 1 + int(epNumber)) + 'v-' + epName + '-episode-' + epNumber)
+        break
+    except:
+        print('Not a valid list of episodes')
+
+with youtube_dl.YoutubeDL() as ydl:
+    ydl.download(epList)
