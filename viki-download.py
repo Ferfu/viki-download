5d40cdbd66b06dd4d9257d381c6e9ef3d3989f9d
George Tian <georgetian3@gmail.com>
2020-05-28 15:31:02 +1000

    Fixed bug where videos with non-consecutively numbered URLs won't download, added ability to download subtitles of multiple languages.

--- viki-download.py
+++ viki-download.py
@@ -1,68 +1,48 @@
 from __future__ import unicode_literals
 import youtube_dl
 
-
+# playlist url
 while True:
-    ep1 = input('Copy + paste the URL of the FIRST episode of the series:\n')
+    url = input('\nEnter the URL of the HOME PAGE of the series (e.g. https://www.viki.com/tv/36667c-hotel-del-luna):\n')
     try:
-        epUrl = ep1[ep1.index('videos/') + 7:]
+        with youtube_dl.YoutubeDL({"playliststart": 0, "playlistend": 0, "quiet": True}) as ydl:
+            ydl.extract_info(url, download=False)
         break
     except:
-        print('Not a valid URL. The URL should be in the following format:\nhttps://www.viki.com/videos/XXXXXXXv-name-of-drama-episode-XX')
+        print("\nInvalid URL\n")
 
-epTag = int(epUrl[:epUrl.index('v-')])
-epName = epUrl[epUrl.index('v-') + 2 : epUrl.index('episode-') - 1]
-
-epList = []
+# episodes
 while True:
-    epNumber = input("List the episode(s) you wish to download in one of the following formats:\n1. Separated by commas, e.g. 1,2,3\n2. Give a range of episodes separated by a dash, e.g 1-3\n")
+    ep = input("\nEnter the first and last episodes to download separated by a space (e.g. 1 16)\n")
     try:
-        if '-' in epNumber:
-            start = int(epNumber[:epNumber.index('-')])
-            end = int(epNumber[epNumber.index('-') + 1:])
-            for i in range(start, end + 1):
-                epList.append('https://www.viki.com/videos/' + str(epTag - 1 + i) + 'v-' + epName + '-episode-' + str(i))
-        elif ',' in epNumber:
-            epNumber = list(epNumber)
-            epNumberList = []
-            for x in epNumber:
-                if x != ' ' and x != ',':
-                    epNumberList.append(int(x))
-            for x in epNumberList:
-                epList.append('https://www.viki.com/videos/' + str(epTag - 1 + x) + 'v-' + epName + '-episode-' + str(x))
-        else:
-            epList.append('https://www.viki.com/videos/' + str(epTag - 1 + int(epNumber)) + 'v-' + epName + '-episode-' + epNumber)
+        start, end = [int(x) for x in ep.split()]
         break
     except:
-        print('Not a valid list of episodes')
+        print('\nInvalid input\n')
 
-sub = False
-subLang = 'en'
+# subtitles
 while True:
-    subIn = input('Download subtitles? [y/n]\n')
-    if subIn == 'y':
-        sub = True
-        while True:
-            subLangIn = input('English, Simplified Chinese, or Indonesian? [e/c/i]\n')
-            if subLangIn == 'e':
-                break
-            elif subLangIn == 'c':
-                subLang = 'zh'
-                break
-            elif subLangIn == 'i':
-                subLang = 'id'
-                break
-            else:
-                print('Invalid input\n')
+    downloadSubs = input("\nDownload subtitles? [y/n]\n")
+    if downloadSubs == "y":
+        downloadSubs = True
         break
-    elif subIn == 'n':
+    elif downloadSubs == "n":
+        downloadSubs = False
         break
     else:
-        print('Invalid input')
+        print("\nInvalid input\n")
+if downloadSubs == True:
+    with youtube_dl.YoutubeDL({"playliststart": end, "playlistend": end, "listsubtitles": True}) as ydl:
+        ydl.extract_info(url, download=False)
+    subLang = input("\nEnter subtitle language. Note some subtitle langauges are not available for all episodes.\n")
 
 
+options = {
+    "writesubtitles": downloadSubs,
+    "subtitleslangs": [subLang],
+    "playliststart": start,
+    "playlistend": end
+}
 
-
-ydl_opts = {"writesubtitles": sub, "subtitleslangs": [subLang]}
-with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-    ydl.download(epList)
+with youtube_dl.YoutubeDL(options) as ydl:
+    ydl.download([url])
