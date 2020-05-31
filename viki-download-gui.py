604a35b7ff3b1edc95e99c1c539b9688b760453c
George Tian <georgetian3@gmail.com>
2020-05-31 17:40:27 +1000

    GUI without login or download

--- viki-download-gui.py
+++ viki-download-gui.py
@@ -1,15 +1,13 @@
 from __future__ import unicode_literals
 import youtube_dl
+import threading
 import tkinter as tk
 import tkinter.filedialog
 import tkinter.messagebox
-import time
+import os
 
-requiredFields = {"url": False, "location": False}
+langDict = {'aa': 'Afar', 'ab': 'Abkhazian', 'ae': 'Avestan', 'af': 'Afrikaans', 'ak': 'Akan', 'am': 'Amharic', 'an': 'Aragonese', 'ar': 'Arabic', 'as': 'Assamese', 'av': 'Avaric', 'ay': 'Aymara', 'az': 'Azerbaijani', 'ba': 'Bashkir', 'be': 'Belarusian', 'bg': 'Bulgarian', 'bh': 'Bihari', 'bm': 'Bambara', 'bi': 'Bislama', 'bn': 'Bengali', 'bo': 'Tibetan', 'br': 'Breton', 'bs': 'Bosnian', 'ca': 'Catalan', 'ce': 'Chechen', 'ch': 'Chamorro', 'co': 'Corsican', 'cr': 'Cree', 'cs': 'Czech', 'cu': 'Church', 'cv': 'Chuvash', 'cy': 'Welsh', 'da': 'Danish', 'de': 'German', 'dv': 'Divehi', 'dz': 'Dzongkha', 'ee': 'Ewe', 'el': 'Greek', 'en': 'English', 'eo': 'Esperanto', 'es': 'Spanish', 'et': 'Estonian', 'eu': 'Basque', 'fa': 'Persian', 'ff': 'Fulah', 'fi': 'Finnish', 'fj': 'Fijian', 'fo': 'Faroese', 'fr': 'French', 'fy': 'Western', 'ga': 'Irish', 'gd': 'Gaelic', 'gl': 'Galician', 'gn': 'Guarani', 'gu': 'Gujarati', 'gv': 'Manx', 'ha': 'Hausa', 'he': 'Hebrew', 'hi': 'Hindi', 'ho': 'Hiri', 'hr': 'Croatian', 'ht': 'Haitian', 'hu': 'Hungarian', 'hy': 'Armenian', 'hz': 'Herero', 'ia': 'Interlingua', 'id': 'Indonesian', 'ie': 'Interlingue', 'ig': 'Igbo', 'ii': 'Sichuan', 'ik': 'Inupiaq', 'io': 'Ido', 'is': 'Icelandic', 'it': 'Italian', 'iu': 'Inuktitut', 'ja': 'Japanese', 'jv': 'Javanese', 'ka': 'Georgian', 'kg': 'Kongo', 'ki': 'Kikuyu', 'kj': 'Kuanyama', 'kk': 'Kazakh', 'kl': 'Kalaallisut', 'km': 'Central', 'kn': 'Kannada', 'ko': 'Korean', 'kr': 'Kanuri', 'ks': 'Kashmiri', 'ku': 'Kurdish', 'kv': 'Komi', 'kw': 'Cornish', 'ky': 'Kirghiz', 'la': 'Latin', 'lb': 'Luxembourgish', 'lg': 'Ganda', 'li': 'Limburgan', 'ln': 'Lingala', 'lo': 'Lao', 'lt': 'Lithuanian', 'lu': 'Luba-Katanga', 'lv': 'Latvian', 'mg': 'Malagasy', 'mh': 'Marshallese', 'mi': 'Maori', 'mk': 'Macedonian', 'ml': 'Malayalam', 'mn': 'Mongolian', 'mne': 'Montenegrin ', 'mo': 'Moldavian', 'mr': 'Marathi', 'ms': 'Malay', 'mt': 'Maltese', 'my': 'Burmese', 'na': 'Nauru', 'nb': 'Bokmål,', 'nd': 'Ndebele,', 'ne': 'Nepali', 'ng': 'Ndonga', 'nl': 'Dutch', 'nn': 'Norwegian', 'no': 'Norwegian', 'nr': 'Ndebele,', 'nv': 'Navajo', 'ny': 'Chichewa', 'oc': 'Occitan', 'oj': 'Ojibwa', 'om': 'Oromo', 'or': 'Oriya', 'os': 'Ossetian', 'pa': 'Panjabi', 'pi': 'Pali', 'pl': 'Polish', 'ps': 'Pushto', 'pt': 'Portuguese', 'qu': 'Quechua', 'rm': 'Romansh', 'rn': 'Rundi', 'ro': 'Romanian', 'ru': 'Russian', 'rw': 'Kinyarwanda', 'sa': 'Sanskrit', 'sc': 'Sardinian', 'sd': 'Sindhi', 'se': 'Northern', 'sg': 'Sango', 'si': 'Sinhala', 'sk': 'Slovak', 'sl': 'Slovenian', 'sm': 'Samoan', 'sn': 'Shona', 'so': 'Somali', 'sq': 'Albanian', 'sr': 'Serbian', 'ss': 'Swati', 'sh': 'Serbo-Croatian', 'st': 'Sotho,', 'su': 'Sundanese', 'sv': 'Swedish', 'sw': 'Swahili', 'ta': 'Tamil', 'te': 'Telugu', 'tg': 'Tajik', 'th': 'Thai', 'ti': 'Tigrinya', 'tk': 'Turkmen', 'tl': 'Tagalog', 'tn': 'Tswana', 'to': 'Tonga', 'tr': 'Turkish', 'ts': 'Tsonga', 'tt': 'Tatar', 'tw': 'Twi', 'ty': 'Tahitian', 'ug': 'Uighur', 'uk': 'Ukrainian', 'ur': 'Urdu', 'uz': 'Uzbek', 've': 'Venda', 'vi': 'Vietnamese', 'vo': 'Volapük', 'wa': 'Walloon', 'wo': 'Wolof', 'xh': 'Xhosa', 'yi': 'Yiddish', 'yo': 'Yoruba', 'za': 'Zhuang', 'zh': 'Chinese Simplified', 'zt': 'Chinese Traditional', 'zu': 'Zulu'}
 
-def debugging():
-    with youtube_dl.YoutubeDL() as ydl:
-        ydl.extract_info(urlEntry.get(), download=False)
 def toggleEntry():
     if login.get():
         usernameEntry.config(state = "normal")
@@ -19,72 +17,109 @@ def toggleEntry():
         passwordEntry.config(state = "disabled")
 def askDirectory():
     location = tk.filedialog.askdirectory()
-    locationButton.config(text = location)
-def checkUrl():
-    urlButton.config(text = "Extracting video information\nThis may take 1 min")
-    time.sleep(10)
-    try:
-        with youtube_dl.YoutubeDL({"playliststart": 0, "playlistend": 0, "quiet": True}) as ydl:
-            ydl.extract_info(urlEntry.get(), download=False)
-        requiredFields["url"] = True
-        getVideoInfo()
-    except:
-        urlButton.config(text = "Check URL")
-        tk.messagebox.showerror(title="Error", message="Invalid URL")
+    locationLabel.config(text = location)
 def getVideoInfo():
+    urlButton.config(text = "Extracting video info", state = "disabled")
+    #try:
     with youtube_dl.YoutubeDL() as ydl:
-        ydl.extract_info(urlEntry.get(), download=False)
+        playlistInfo = ydl.extract_info(urlEntry.get(), download=False)
+        i = 1
+        for video in playlistInfo['entries']:
+            for subtitle in video.get('subtitles').keys():
+                if langDict[subtitle] not in subtitleList:
+                    subtitleList.append(langDict[subtitle])
+            videoList[video.get('title')] = [i, tk.BooleanVar()]
+            i += 1
+        subtitleList.sort()
+        requiredFields["url"] = True
+    for video in videoList:
+        print(video, videoList[video][0], videoList[video][1].get())
+    '''
+    except:
+        tk.messagebox.showerror(title="Error", message="Invalid URL")
+    '''
+    urlButton.config(text = "Extract video info", state = "normal")
+    for video in videoList:
+        tk.Checkbutton(videoListFrame, text = video, var = videoList[video][1]).pack(anchor = "w")
+    subtitleList.insert(0, "No subtitle")
+    subtitleSelected.set(subtitleList[0])
+    subtitleMenu = tk.OptionMenu(subtitleFrame, subtitleSelected, *subtitleList).pack()
+def selectAll():
+    for video in videoList:
+        videoList[video[1]].set(True)
+def deselectAll():
+    for video in videoList:
+        videoList[video[1]].set(False)
+def download():
+    videoSelected = []
+    for video in videoList:
+        if videoList[video][1].get():
+            videoSelected.append(videoList[video][0])
+    print(videoSelected)
+    downloadButton.config(text = "Downloading. Check terminal for progerss", state = "disabled")
+    '''
+    ydl_options = {
+        "playlist-items": ,
+        "subtitles": ,
+        "outmpl"
+    }
+
+    with youtube_dl.YoutubeDL() as ydl:
+        ydl.download([urlEntry.get()])
+    '''
 
 m = tk.Tk()
 m.geometry()
-
 m.title("viki-download-gui")
+requiredFields = {"url": False}
+videoList = {}
+subtitleList = []
+location = os.getcwd()
 
+loginFrame = tk.Frame(m)
+urlFrame = tk.Frame(m)
+videoFrame = tk.Frame(m)
+videoListFrame = tk.Frame(m, height=300, width=300)
+subtitleFrame = tk.Frame(m)
+locationFrame = tk.Frame(m)
+downloadFrame = tk.Frame(m)
+'''
 login = tk.BooleanVar()
-loginCheck = tk.Checkbutton(m, text = "Login to Viki account", var = login, command = toggleEntry)
+loginCheck = tk.Checkbutton(m, text = "Login to Viki account", var = login, command = toggleEntry).grid(row = 0, column = 0)
+usernameLabel = tk.Label(loginFrame, text = "Username").grid(row = 0, column = 0)
+usernameEntry = tk.Entry(loginFrame, state = "disabled")
+usernameEntry.grid(row = 0, column = 1)
+passwordLabel = tk.Label(loginFrame, text = "Password").grid(row = 1, column = 0)
+passwordEntry = tk.Entry(loginFrame, state = "disabled")
+passwordEntry.grid(row = 1, column = 1)
+'''
+urlLabel = tk.Label(m, text = "Drama homepage URL").grid(row = 1, column = 0)
+urlEntry = tk.Entry(urlFrame)
+urlEntry.pack(side = tk.LEFT)
+urlButton = tk.Button(urlFrame, text = "Extract video info", command = lambda: threading.Thread(target = getVideoInfo).start())
+urlButton.pack(side = tk.LEFT)
 
-usernameLabel = tk.Label(text = "Username")
-usernameEntry = tk.Entry(state = "disabled")
+videoLabel = tk.Label(videoFrame, text = "Select videos").pack()
+selectAll = tk.Button(videoFrame, text = "Select all", command = selectAll).pack()
+deselectAll = tk.Button(videoFrame, text = "Deselect all", command = deselectAll).pack()
 
-passwordLabel = tk.Label(text = "Password")
-passwordEntry = tk.Entry(state = "disabled")
+subtitleLabel = tk.Label(m, text = "Select subtitle").grid(row = 3, column = 0)
+subtitleSelected = tk.StringVar()
 
-urlLabel = tk.Label(text = "URL of HOME PAGE of drama\ne.g. https://www.viki.com/tv/36667c-hotel-del-luna")
-urlEntry = tk.Entry()
-urlButton = tk.Button(text = "Check URL", command = checkUrl)
-
-videoLabel = tk.Label(text = "Select videos")
-subtitleLabel = tk.Label(text = "Select subtitles")
-locationLabel = tk.Label(text = "Select download location")
-location = "Select download location"
-locationButton = tk.Button(text = location, command = askDirectory)
-
-downloadButton = tk.Button(text = "Start Download")
-
-
-
-
-
-
-loginCheck.grid(row = 0, column = 0)
-
-usernameLabel.grid(row = 1, column = 0)
-usernameEntry.grid(row = 1, column = 1)
-
-passwordLabel.grid(row = 2, column = 0)
-passwordEntry.grid(row = 2, column = 1)
-
-urlLabel.grid(row = 3, column = 0)
-urlEntry.grid(row = 3, column = 1)
-urlButton.grid(row = 4, column = 1)
-
-videoLabel.grid(row = 5)
-subtitleLabel.grid(row = 6, column = 0)
-locationLabel.grid(row = 7, column = 0)
-locationButton.grid(row = 7, column = 1)
-
-downloadButton.grid(row = 9)
+locationButton = tk.Button(m, text = "Select download location", command = askDirectory).grid(row = 4, column = 0)
+locationLabel = tk.Label(locationFrame, text = location)
+locationLabel.pack()
 
 
+downloadButton = tk.Button(m, text = "Start Download", command = lambda: threading.Thread(target = download).start())
+downloadButton.grid(row = 5, columnspan = 2)
+
+#loginFrame.grid(row = 0, column = 1)
+urlFrame.grid(row = 1, column = 1)
+videoFrame.grid(row = 2, column = 0)
+videoListFrame.grid(row = 2, column = 1)
+subtitleFrame.grid(row = 3, column = 1)
+locationFrame.grid(row = 4, column = 1)
+downloadFrame.grid(row = 5, column = 1)
 
 m.mainloop()
